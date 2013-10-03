30775d1466ad274df08a1aecca40c6fd9fb8fd6d
Kamil Figiela <kf@olorin.info>
2013-10-03 07:00:07 -0400

    Initial commit

---
+++ examples/3_instrukcje_warunkowe.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+liczba = int(raw_input("Podaj liczbę: "))
+
+if liczba >= 10:
+    print "Liczba jest większa lub równa 10"
+else:
+    print "Liczba jest mniejsza od 10"
---
+++ examples/4_petla_for.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+ile = int(raw_input("Podaj liczbę liczb: "))
+
+for i in xrange(0, ile):
+    print i
